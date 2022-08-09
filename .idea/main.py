f66201de9ad6db794d5ba83a48f0eaf63b36d625
Kelsey Perkins <kperkins@xpanxion.com>
2022-08-09 08:55:04 -0700

    second

--- .idea/main.py
+++ .idea/main.py
@@ -8,10 +8,23 @@
 
 import data_access_s3
 import boto3
+import data_access_s3
+from botocore.exceptions import ClientError
+
 
 def main():
     # Upload
     print(data_access_s3.upload_to_s3())
+
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file("uploadme.txt", 'siu-avb-bucket', 'upload_stu7_file.txt')
+    except ClientError as e:
+        print(e)
+        return False
+    return True
+
+
 
     # Download
     # data_access_s3.download_from_s3()
